7d881f339611ad8f119973b8a9aaa945ee6ee50a
姚嘉松 <yaojiasong@youzan.com>
2019-03-28 14:46:58 +0800

    fix: setDtata

--- miniprogram/pages/chart/chart.js
+++ miniprogram/pages/chart/chart.js
@@ -1,2 +1,13 @@
 const app = getApp();
-Page({});
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
+        })
+      }
+    }
+  }
+});
