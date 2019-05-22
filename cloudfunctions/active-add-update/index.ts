294680f1f5051ce8638c2a74690316c509c9bcf6
姚嘉松 <yaojiasong@youzan.com>
2019-05-22 23:36:27 +0800

    fix: add cloud functions

---
+++ cloudfunctions/active-add-update/index.ts
@@ -0,0 +1,16 @@
+import cloud from 'wx-server-sdk';
+
+
+cloud.init()
+
+
+export const main = async (event) => {
+  const wxContext = cloud.getWXContext()
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
